76461b97b55e3cb9e94583eac29f5c39b38da50a
Anna Karutina <anna.karutina@khk.ee>
2019-09-23 11:59:20 +0300

    HTML väljastus koos PHP-ga

--- 23.09.2019/print.php
+++ 23.09.2019/print.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
 // printimine print funktsiooni abil
 print "See on esimene php rida<br>";
@@ -10,3 +20,6 @@ print "See on kolmas php rida<br>";
  * */
 // echo abil printimine
 echo 'See on tekst, mis kirjutatud echo abil';
+?>
+</body>
+</html>
