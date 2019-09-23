555d7f7ce6f92ce924bac14bc6222adcc9a0d3dd
Anna Karutina <anna.karutina@khk.ee>
2019-09-23 12:43:04 +0300

    Väärtuse loomine avaldise abil

--- 23.09.2019/ex_2.php
+++ 23.09.2019/ex_2.php
@@ -20,3 +20,9 @@ echo 'Perenimi: '.$pereNimi.'<br>';
 echo 'Vanus: '.$vanus.'<br>';
 echo 'Pikkus: '.$pikkus.'<br>';
 echo 'Kaal: '.$kaal.'<br>';
+
+// arvutused
+// $muutuja  = väärtus;
+$kmi = $kaal / ($pikkus * $pikkus);
+echo 'Minu kehamassiindeks: '.$kmi.'<br>';
+
