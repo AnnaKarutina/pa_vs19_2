50c4184e32a3879ad8c9eeeb6d20a353231f2848
Anna Karutina <anna.karutina@khk.ee>
2019-09-23 12:37:28 +0300

    Ülesanne 2 - võimalik lahendus

--- 23.09.2019/ex_2.php
+++ 23.09.2019/ex_2.php
@@ -8,3 +8,15 @@
  * Kaal kilogrammides
  * Väljasta kõik vajalik info ka!
  * */
+$eesNimi = 'Anna';
+$pereNimi = 'Karutina';
+$vanus = 38;
+$pikkus = 1.62;
+$kaal = 54;
+
+echo '<h1>Minu andmed</h1>';
+echo 'Eesnimi: '.$eesNimi.'<br>';
+echo 'Perenimi: '.$pereNimi.'<br>';
+echo 'Vanus: '.$vanus.'<br>';
+echo 'Pikkus: '.$pikkus.'<br>';
+echo 'Kaal: '.$kaal.'<br>';
