6245a4976d8d95c3a2c0db6b74dd36d26c20b841
Anna Karutina <anna.karutina@khk.ee>
2019-09-23 12:26:14 +0300

    Muutujate ja andmetüübide põhikasutus

---
+++ 23.09.2019/variables.php
@@ -0,0 +1,20 @@
+<?php
+// muutujate defineerimine + väärtuse omistamine
+// $muutujaNimi = väärtus;
+//
+// Andmetüübid
+// Sõned - String
+// "teksti kujulised andmed"
+// 'või niisuguste jutumärkide vahel ka'
+// Numbrid - täisarvud - 1, 2, 175, -25, jne
+// Numbrid - reaalarvud - 1.5 , -2.89 jne
+
+$eesNimi = "Anna";
+$pereNimi = "Karutina";
+$yksus = "IKT osakond";
+$email = "anna.karutina@khk.ee";
+
+// muutujate kasutamine - kutsumine
+echo $eesNimi." ".$pereNimi."<br>";
+echo $yksus."<br>";
+echo $email."<br>";
