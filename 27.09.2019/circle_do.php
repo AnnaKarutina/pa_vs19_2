6df39c29a09a5c53205f467f17f4ac6456cc1875
Anna Karutina <anna.karutina@khk.ee>
2019-09-27 15:33:36 +0300

    PI väärtuse arvutamise ja ümmardamisfunktsiooni kasutamine

--- 27.09.2019/circle_do.php
+++ 27.09.2019/circle_do.php
@@ -4,8 +4,12 @@ $circleRadius = $_GET['circle-radius'];
 // $circleRadius sisu kontroll
 //var_dump($circleRadius);
 // arvutused
-$circleArea = 3.14 * $circleRadius * $circleRadius;
-$circleCircumference = 3.14 * 2 * $circleRadius;
+$circleArea = pi() * $circleRadius * $circleRadius;
+$circleCircumference = pi() * 2 * $circleRadius;
+// ümmardamine ja omistamine
+//$circleAreaRounded = round($circleArea, 3);
+// ümmardamine ja ümber kirjutamine
+$circleArea = round($circleArea, 3);
 // print
 echo 'Raadius = '.$circleRadius.'<br>';
 echo '<hr>';
