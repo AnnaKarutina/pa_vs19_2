179ecd35e5f39e1062fdf18b8a612d868dc8c95a
Anna Karutina <anna.karutina@khk.ee>
2019-10-16 15:46:51 +0300

    tärniveerg

---
+++ 16.10.2019/asteriks_column.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * file name: asteriks_column.php;
+ * autor: anna.karutina;
+ * date: 16.10.2019;
+ */
+// for(tjm = alg; tjm <= lopp; tjm++)
+// for(tjm = lopp; tjm > alg; tjm--)
+for($count = 1; $count <= 25; $count++){
+  echo $count;
+  if($count < 10) {
+    echo '&nbsp;&nbsp;&nbsp;';
+  } else {
+    echo '&nbsp;';
+  }
+  echo '*<br>';
+}
+
