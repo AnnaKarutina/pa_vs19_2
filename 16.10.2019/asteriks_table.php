b8f1a8ffad0677c2383027577fb236e2f93756cd
Anna Karutina <anna.karutina@khk.ee>
2019-10-16 16:03:09 +0300

    tärnikolmnurk - 2

--- 16.10.2019/asteriks_table.php
+++ 16.10.2019/asteriks_table.php
@@ -6,6 +6,11 @@
  */
 for($row = 1; $row < 6; $row++) {
   echo $row;
+//  spaces
+  for($col = 1; $col < (6-$row); $col++) {
+    echo '&nbsp;&nbsp;&nbsp;';
+  }
+//  asterikses
   for($col = 1; $col <= $row; $col++) {
     echo '*&nbsp;';
   }
