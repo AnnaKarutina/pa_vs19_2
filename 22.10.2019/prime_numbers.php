1c61efc937ad1a6ef6cc66d30a7cfef553d36aef
Anna Karutina <anna.karutina@khk.ee>
2019-10-22 16:01:19 +0300

    algarvu tuvastamine - koos return operaatoriga

--- 22.10.2019/prime_numbers.php
+++ 22.10.2019/prime_numbers.php
@@ -7,18 +7,19 @@
 // define function
 function isPrime($number){
   if($number < 2) {
-    echo 'This number is not valid';
+    return 'This number is not valid';
   } else {
     $divider = 2;
     while ($number % $divider != 0) {
       $divider++;
     }
     if($number == $divider) {
-      echo $number.' is prime number<br>';
+      return $number.' is prime number<br>';
     } else {
-      echo $number.' is not a prime number<br>';
+      return $number.' is not a prime number<br>';
     }
   }
 }
 // use function
-isPrime(rand(0, 99));
+$result = isPrime(rand(0, 99));
+echo $result;
